66e04597693b9beed28001d41c59fdce6f210a72
DrWisWis <ibiapinoigor@gmail.com>
2025-11-06 12:45:11 -0300

    create: cnx bd

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = ''; // senha do seu MySQL
+$dbname = 'MockyCode';
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+?>
