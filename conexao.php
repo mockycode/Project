b1f15a7d369150c376c5aa1409c93be9f736763b
DrWisWis <ibiapinoigor@gmail.com>
2025-11-19 00:09:53 -0300

    Fix: Conexao to DevStation

--- conexao.php
+++ conexao.php
@@ -1,14 +1,29 @@
 <?php
-$host=getenv("DB_HOST");
-$port=getenv("DB_PORT");
-$socket="";
-$user=getenv("DB_USER");
-$password=getenv("DB_PASS");; 
-$dbname=getenv("DB_NAME");
+
+$configPath = _DIR_ . "/config.php";
+
+if (file_exists($configPath)) {
+    // Dev Station
+    include $configPath;
+    $host = $db_host;
+	$port = $db_port;
+	$socket="";
+    $password = $db_password;
+    $name = $db_name;
+	$user = $db_user;
+} else {
+    // FLY.IO
+	$host=getenv("DB_HOST");
+	$port=getenv("DB_PORT");
+	$socket="";
+	$user=getenv("DB_USER");
+	$password=getenv("DB_PASS");; 
+	$dbname=getenv("DB_NAME");
+}
 
 $conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 
 //$conn->close();
- 
+ 
 ?>
